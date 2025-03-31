de57e630d285af8101891d43292d6bf6ad610857
BramM15 <bramfabian2006@gmail.com>
2025-03-31 14:57:43 +0200

    setup
    Crashout

--- src/main/java/com/github/bramm15/undertail/Undertail.java
+++ src/main/java/com/github/bramm15/undertail/Undertail.java
@@ -8,7 +8,17 @@ import com.github.hanyaeger.api.YaegerGame;
  */
 public class Undertail extends YaegerGame
 {
-    public static void main( String[] args )
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+
+    public static void main(String[] args )
     {
         System.out.println( "Hello World!" );
     }
