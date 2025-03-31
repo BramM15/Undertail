cc61ad461a4cad12df103c41610eafb1366fb7e3
Ruben-Haverkamp <teddergamer130@gmail.com>
2025-03-31 14:39:17 +0200

    Setup

---
+++ src/main/java/com/github/bramm15/undertail/Undertail.java
@@ -0,0 +1,15 @@
+package com.github.bramm15.undertail;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+/**
+ * Hello world!
+ *
+ */
+public class Undertail extends YaegerGame
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
